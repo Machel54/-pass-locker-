5a4d6c0dc04bd5be47ae420162b0b20935213f92
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-10-06 22:37:20 +0300

    use test_save_user to save the user object into user_list

--- user_test.py
+++ user_test.py
@@ -26,5 +26,12 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.username,"Machel")
         self.assertEqual(self.new_user.password,"mk11")
 
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saves into the user list
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+
 if __name__ == '__main__':
-    unittest.main()
+     unittest.main()
