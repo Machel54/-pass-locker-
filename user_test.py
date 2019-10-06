19379f22aa165105e384036647a38a201d96c0b4
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-10-06 16:42:21 +0300

    use setUp method to run each test cases

--- user_test.py
+++ user_test.py
@@ -4,7 +4,14 @@ from user import User
 class TestUser(unittest.TestCase):
   '''
   Test class that defines test cases for the user class behaviours
-
   Args:
       unittest.TestCase: TestCase class that helps in creating test cases.
   '''
+def setUp(self):
+  '''
+  set up method to run before each test cases.
+  '''
+  self.new_user("Logue","smash") 
+
+if __name__ == '__main__':
+  unittest.main()
