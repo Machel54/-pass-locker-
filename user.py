4c0fdf9153f8c31264299804933fe27b7e075231
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-10-06 14:37:32 +0300

    create a  class that generates users

---
+++ user.py
@@ -0,0 +1,9 @@
+class User:
+  '''
+  Class that generates new users
+  '''
+  user_list = [] #Empty user list
+
+def _init_(self, username, password):
+  self.username = username
+  self.password = password
