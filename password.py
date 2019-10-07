c59e6dfc02f3b79b31667aab9867c359afda054d
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-10-07 17:22:25 +0300

    Create function to create new user

---
+++ password.py
@@ -0,0 +1,10 @@
+#/usr/bin/env python3.6
+from user import User
+
+def create_user(user_name,password):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(user_name,password)
+    return new_user
+
